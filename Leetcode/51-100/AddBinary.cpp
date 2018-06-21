8630d226e64d4661144088cb3fca760eb1594780
Neal Haonan Chen <hc4pa@virginia.edu>
2018-06-20 23:53:31 -0400

    Create AddBinary.cpp

---
+++ Leetcode/51-100/AddBinary.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        
+        int am = a.length();
+        int bm = b.length();
+        if(am > bm)string ret = a;
+        else string.ret = b;
+        
+        int m = min(am,bm);
+        int index = 0;
+        int next = 0;
+        while(index <= m){
+            char c = a[am - index] + b[bm - index] + next;
+            if(c = '0')
+            else if(c )
+            
+            
+            
+            
+            
+        index ++;
+        }
+        
+        
+        
+    }
+};
